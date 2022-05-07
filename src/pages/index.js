80812a5e9953cdc5af1f444efc3551345c324f04
JakeerC <jakeerchilakala@gmail.com>
2022-05-07 10:38:04 +0530

    chore: nextjs setup

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Jakeer</title>
+        <meta name="description" content="Blog and Portfolio site of Jakeer" />
+        <link rel="icon" href="/favicon.ico" />
+        <main> </main>
+      </Head>
+    </div>
+  );
+}
