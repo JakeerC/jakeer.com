24c1e186ad996d4f227b1a73c2e40cc27d4f4c4e
JakeerC <jakeerchilakala@gmail.com>
2022-05-07 10:41:44 +0530

    build: setup eslint , prettier

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  jsxSingleQuote: false,
+  printWidth: 80,
+  arrowParens: 'avoid',
+  endOfLine: require('os').EOL === `r\n` ? 'crlf' : 'lf',
+};
