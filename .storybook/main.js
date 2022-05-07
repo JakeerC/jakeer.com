fa2011cfa10c6a91bbcdfe1cf7b77c49fe2a8eb1
JakeerC <jakeerchilakala@gmail.com>
2022-05-07 15:58:27 +0530

    docs: add storybook and configure

---
+++ .storybook/main.js
@@ -0,0 +1,11 @@
+module.exports = {
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  /** Expose public folder to storybook as static */
+  staticDirs: ['../public'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+  ],
+  framework: '@storybook/react',
+};
