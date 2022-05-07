f6967ef2b91705aca67b0dc9b94fb748b2b8373a
JakeerC <jakeerchilakala@gmail.com>
2022-05-07 11:03:23 +0530

    build: add git-hooks using husky

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import PropTypes from 'prop-types';
+
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
+
+MyApp.propTypes = {
+  Component: PropTypes.element,
+  pageProps: PropTypes.any,
+};
