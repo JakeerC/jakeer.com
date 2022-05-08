3c28149f2e03a21f2f88eb64759eba3f541e5286
JakeerC <jakeerchilakala@gmail.com>
2022-05-08 23:04:46 +0530

    chore: setup tailwind for theme toggle and add related styles in globalcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,61 @@
+const { fontFamily } = require('tailwindcss/defaultTheme');
+
+function withOpacity(variableName) {
+  return ({ opacityValue }) => {
+    if (opacityValue !== undefined) {
+      return `rgba(var(${variableName}), ${opacityValue})`;
+    }
+    return `rgb(var(${variableName}))`;
+  };
+}
+
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        primary: ['Inter', ...fontFamily.sans],
+      },
+      colors: {
+        primary: {
+          // Customize it on globals.css :root
+          200: withOpacity('--tw-clr-primary-200'),
+          300: withOpacity('--tw-clr-primary-300'),
+          400: withOpacity('--tw-clr-primary-400'),
+          500: withOpacity('--tw-clr-primary-500'),
+        },
+        dark: '#071524',
+      },
+      keyframes: {
+        flicker: {
+          '0%, 19.999%, 22%, 62.999%, 64%, 64.999%, 70%, 100%': {
+            opacity: 0.99,
+            filter:
+              'drop-shadow(0 0 1px rgba(252, 211, 77)) drop-shadow(0 0 15px rgba(245, 158, 11)) drop-shadow(0 0 1px rgba(252, 211, 77))',
+          },
+          '20%, 21.999%, 63%, 63.999%, 65%, 69.999%': {
+            opacity: 0.4,
+            filter: 'none',
+          },
+        },
+        tilt: {
+          '0%, 50%, 100%': {
+            transform: 'rotate(0deg)',
+          },
+          '25%': {
+            transform: 'rotate(0.5deg)',
+          },
+          '75%': {
+            transform: 'rotate(-0.5deg)',
+          },
+        },
+      },
+      animation: {
+        flicker: 'flicker 3s linear infinite',
+        tilt: 'tilt 10s infinite linear',
+      },
+    },
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
 };
