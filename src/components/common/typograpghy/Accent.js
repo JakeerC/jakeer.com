47a9cfbc775a5404debf390ef2cb26ca26d8549a
JakeerC <jakeerchilakala@gmail.com>
2022-05-12 11:26:35 +0530

    style: change accent  styles

--- src/components/common/typograpghy/Accent.js
+++ src/components/common/typograpghy/Accent.js
@@ -7,8 +7,8 @@ export default function Accent({ children, className }) {
       className={clsx(
         className,
         'transition-colors',
-        'bg-gradient-to-tr from-primary-300/40 via-primary-300/40 to-primary-400/40',
-        'dark:from-primary-300 dark:to-primary-400 dark:bg-clip-text dark:text-transparent'
+        'bg-gradient-to-tr from-primary-300 via-primary-300 to-primary-400 bg-clip-text text-transparent',
+        'dark:from-primary-200 dark:via-primary-300 dark:to-primary-500 dark:bg-clip-text dark:text-transparent'
       )}
     >
       {children}
